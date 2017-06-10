543129796f2f180431bdcd98c3311a56b9292c07
Alex Saunders <acsauk@gmail.com>
2017-06-10 16:43:52 +0100

    updates test dependencies and switches to extending DuskTestCase

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -4,12 +4,13 @@ namespace Tests\Feature;
 
 use App\Concert;
 use Carbon\Carbon;
-use Tests\TestCase;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ViewConcertListingTest extends TestCase
+class ViewConcertListingTest extends DuskTestCase
 {
   use DatabaseMigrations;
   /** @test */
