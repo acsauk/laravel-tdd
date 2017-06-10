cce375b12081d9f0d916f5a600d0c91240ec57f2
Alex Saunders <acsauk@gmail.com>
2017-06-10 15:18:10 +0100

    creates boilerplate test for user adding a concert

---
+++ tests/Feature/ViewConcertListingTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewConcertListingTest extends TestCase
+{
+  /** @test */
+  function user_can_view_a_concert_listing()
+  {
+    // Arrange
+      // Create concert
+
+    // Act
+      // View concert Listing
+
+    // Assert
+      // See concert details
+  }
+}
