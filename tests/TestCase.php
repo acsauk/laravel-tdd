1faebedf3f3219505e153ac26da5bcc2a2b23d98
Alex Saunders <acsauk@gmail.com>
2017-06-11 12:13:46 +0100

    fixes dependency for BaseTestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
-use Laravel\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
